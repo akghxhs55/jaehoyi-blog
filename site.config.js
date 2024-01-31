61b99c3755eddfb6c548ec3eeace8328fe816f34
Jaeho Yi <akghxhs55@gmail.com>
2024-01-31 20:14:29 +0900

    Update site.config.js before deploy

--- site.config.js
+++ site.config.js
@@ -1,25 +1,25 @@
 const CONFIG = {
   // profile setting (required)
   profile: {
-    name: "morethanmin",
-    image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
-    role: "frontend developer",
-    bio: "I develop everything using node.",
-    email: "morethanmin.dev@gmail.com",
-    linkedin: "morethanmin",
-    github: "morethanmin",
+    name: "Jaeho Yi",
+    image: "/avatar.svg",
+    role: "",
+    bio: "",
+    email: "akghxhs55@gmail.com",
+    linkedin: "jaehoyi",
+    github: "akghxhs55",
     instagram: "",
   },
   projects: [
-    {
-      name: `morethan-log`,
-      href: "https://github.com/morethanmin/morethan-log",
-    },
+    // {
+    //   name: `morethan-log`,
+    //   href: "https://github.com/morethanmin/morethan-log",
+    // },
   ],
   // blog setting (required)
   blog: {
-    title: "morethan-log",
-    description: "welcome to morethan-log!",
+    title: "Jaeho Yi",
+    description: "Jaeho Yi's blog",
   },
 
   // CONFIG configration (required)
