a0228a560862b3778a13c85d6ae55c69adde32cb
Jaeho Yi <akghxhs55@gmail.com>
2024-10-13 15:19:17 +0900

    Remove thumbnail in post header

--- src/routes/Detail/PostDetail/PostHeader.tsx
+++ src/routes/Detail/PostDetail/PostHeader.tsx
@@ -48,16 +48,16 @@ const PostHeader: React.FC<Props> = ({ data }) => {
               </div>
             )}
           </div>
-          {data.thumbnail && (
-            <div className="thumbnail">
-              <Image
-                src={data.thumbnail}
-                css={{ objectFit: "contain" }}
-                fill
-                alt={data.title}
-              />
-            </div>
-          )}
+          {/*{data.thumbnail && (*/}
+          {/*  <div className="thumbnail">*/}
+          {/*    <Image*/}
+          {/*      src={data.thumbnail}*/}
+          {/*      css={{ objectFit: "contain" }}*/}
+          {/*      fill*/}
+          {/*      alt={data.title}*/}
+          {/*    />*/}
+          {/*  </div>*/}
+          {/*)}*/}
         </nav>
       )}
     </StyledWrapper>
