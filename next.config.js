7b821c18b46cc395d1708e30f3821b82d88ad915
Jaeho Yi <akghxhs55@gmail.com>
2025-10-26 15:21:24 +0900

    🐛: Fix

--- next.config.js
+++ next.config.js
@@ -2,16 +2,22 @@ module.exports = {
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'www.notion.so'
+        protocol: "https",
+        hostname: "**.notion.so",
       },
       {
-        protocol: 'https',
-        hostname: 'lh5.googleusercontent.com'
+        protocol: "https",
+        hostname: "**.amazonaws.com",
       },
       {
-        protocol: 'https',
-        hostname: 's3-us-west-2.amazonaws.com'
-      }],
+        protocol: "https",
+        hostname: "lh5.googleusercontent.com",
+      },
+    ],
   },
-}
+  serverExternalPackages: [
+    "notion-client",
+    "notion-types",
+    "notion-utils",
+  ],
+};
