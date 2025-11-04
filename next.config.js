673df5c4c6c6208dbeb59c4f11fed3eda0cede3a
Jaeho Yi <akghxhs55@gmail.com>
2025-11-04 20:34:29 +0900

    ✨: Add Global HTTP Security and Cache Headers in Next.js

--- next.config.js
+++ next.config.js
@@ -48,4 +48,16 @@ module.exports = {
       },
     ]
   },
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          { key: "X-Content-Type-Options", value: "nosniff" },
+          { key: "Referrer-Policy", value: "strict-origin-when-cross-origin" },
+          // Add CSP if needed (whitelist Utterances/Notion/Analytics domains)
+        ],
+      },
+    ]
+  },
 };
